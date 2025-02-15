05449a2c1c98dc64c8899f43e6e1d20b80f85cc0
Jez-er <andrey.chernenko008@gmail.com>
2025-02-15 11:53:10 +0200

    [FIX] Builder

---
+++ dist/core/index.d.ts
@@ -0,0 +1,1 @@
+export declare function strixInit(): void;
--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,23 @@
 import typescript from 'rollup-plugin-typescript2'
 
 export default {
-	input: './src/index.ts', // Входной файл
+	input: './src/index.ts',
 	output: [
 		{
-			file: 'dist/bundle.cjs.js', // CommonJS
+			file: 'dist/bundle.cjs.js',
 			format: 'cjs',
 			sourcemap: true,
 		},
 		{
-			file: 'dist/bundle.esm.js', // ES Module
+			file: 'dist/bundle.esm.js',
 			format: 'esm',
 			sourcemap: true,
 		},
 	],
 	plugins: [
-		typescript(), // Плагин для TypeScript
+		typescript({
+			tsconfig: './tsconfig.json', // Убедитесь, что используется правильный tsconfig
+			useTsconfigDeclarationDir: true, // Это заставит генерировать декларации типов в папке dist
+		}),
 	],
-	// external: ['express'], // Укажи внешние зависимости
 }
